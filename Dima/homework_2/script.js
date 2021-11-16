e217755c97351c36e4a66ffb7be1ac9a3e5462ce
Dmitry Luzko <vilkasts@gmail.com>
2021-11-16 21:58:18 +0500

    add new homework special for you!!❤

--- Dima/homework_2/script.js
+++ Dima/homework_2/script.js
@@ -1,1 +1,78 @@
-console.log('novaja domashka');
+// let a = 1 + 2 + 3;
+// alert(a);
+
+// alert(1 + 2 +3);
+
+// let a = 10;
+// let b = 2;
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a / b);
+
+// let c = 10;
+// let d = 5;
+// result = c + d;
+// alert(result);
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// alert(a + b + c);
+
+// let a = 10;
+// let b = 5;
+// let c = a - b;
+// let d = 7;
+// let result = c + d;
+// alert(result);
+
+// vivedet 20; //
+
+// vivedet 23; //
+
+// vivedet 6; //
+
+// vivedet 9; //
+
+// vivedet 8; //
+
+// vivedet 8; //
+
+// vivedet 25; //
+
+// vivedet 13; //
+
+// vivedet 36; //
+
+// vivedet 4; //
+
+// vivedet 4; //
+
+// vivedet 4; //
+
+// let a = 1.5;
+// let b = 0.75;
+// alert(a + b);
+
+// let a = -100;
+// alert(a);
+
+// let a = 27.324325;
+// alert(-a);
+
+// let a = 10;
+// let b = 3;
+// alert(a % b);
+
+// alert(2 ** 10);
+
+// vivedet 24; //
+
+// vivedet 216; //
+
+// vivedet 48; //
+
+// vivedet 24; //
+
+// vivedet 72; //
